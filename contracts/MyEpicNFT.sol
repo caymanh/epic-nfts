11884ec98b84566e8fa19b0eef2005084cf82c5a
Cayman Heng <hengcayman@gmail.com>
2023-02-24 14:29:05 +0800

    updated contract with base64 encoded JSON

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -9,37 +9,49 @@ import "hardhat/console.sol";
 
 // inherit the we imported and access the inherited contract's methods.
 contract MyEpicNFT is ERC721 {
-  // keep track of tokenIds with OpenZeppelin 
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+    // keep track of tokenIds with OpenZeppelin
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
-  // pass the name of NFTs token and its symbol.
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract. Woah!");
-  }
+    // pass the name of NFTs token and its symbol.
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is my NFT contract. Woah!");
+    }
 
-  // A function user will hit to get the NFT.
-  function makeAnEpicNFT() public {
-     // Get the current tokenId; starts at 0.
-    uint256 newItemId = _tokenIds.current();
+    // A function user will hit to get the NFT.
+    function makeAnEpicNFT() public {
+        // Get the current tokenId; starts at 0.
+        uint256 newItemId = _tokenIds.current();
 
-     // mint the NFT to the sender using msg.sender.
-    _safeMint(msg.sender, newItemId);
-    
-    // Return the NFT's metadata
-    tokenURI(newItemId);
+        // mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
 
-    // Increment the counter for when the next NFT is minted.
-    _tokenIds.increment();
-  }
+        // Return the NFT's metadata
+        tokenURI(newItemId);
 
-  // Set the NFT's metadata
-  function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-    require(_exists(_tokenId));
-    console.log("An NFT w/ ID %s has been minted to %s", _tokenId, msg.sender);
-    return string(
-    abi.encodePacked(
-        "data:application/json;base64,",
-        "ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTB4dmNtUklZVzFpZFhKblpYSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSIKfQ=="
-    )
-)
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
+
+    // Set the NFT's metadata
+    function tokenURI(uint256 _tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        require(_exists(_tokenId));
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            _tokenId,
+            msg.sender
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    "ewogICAgIm5hbWUiOiAiRXBpY0xvcmRIYW1idXJnZXIiLAogICAgImRlc2NyaXB0aW9uIjogIkFuIE5GVCBmcm9tIHRoZSBoaWdobHkgYWNjbGFpbWVkIHNxdWFyZSBjb2xsZWN0aW9uIiwKICAgICJpbWFnZSI6ICJkYXRhOmltYWdlL3N2Zyt4bWw7YmFzZTY0LFBITjJaeUI0Yld4dWN6MGlhSFIwY0RvdkwzZDNkeTUzTXk1dmNtY3ZNakF3TUM5emRtY2lJSEJ5WlhObGNuWmxRWE53WldOMFVtRjBhVzg5SW5oTmFXNVpUV2x1SUcxbFpYUWlJSFpwWlhkQ2IzZzlJakFnTUNBek5UQWdNelV3SWo0S0lDQWdJRHh6ZEhsc1pUNHVZbUZ6WlNCN0lHWnBiR3c2SUhkb2FYUmxPeUJtYjI1MExXWmhiV2xzZVRvZ2MyVnlhV1k3SUdadmJuUXRjMmw2WlRvZ01UUndlRHNnZlR3dmMzUjViR1UrQ2lBZ0lDQThjbVZqZENCM2FXUjBhRDBpTVRBd0pTSWdhR1ZwWjJoMFBTSXhNREFsSWlCbWFXeHNQU0ppYkdGamF5SWdMejRLSUNBZ0lEeDBaWGgwSUhnOUlqVXdKU0lnZVQwaU5UQWxJaUJqYkdGemN6MGlZbUZ6WlNJZ1pHOXRhVzVoYm5RdFltRnpaV3hwYm1VOUltMXBaR1JzWlNJZ2RHVjRkQzFoYm1Ob2IzSTlJbTFwWkdSc1pTSStSWEJwWTB4dmNtUklZVzFpZFhKblpYSThMM1JsZUhRK0Nqd3ZjM1puUGc9PSIKfQ=="
+                )
+            );
+    }
+}
